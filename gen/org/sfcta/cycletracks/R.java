e112a1ea7a72a137e163cdc4c592a61361f3b4e4
Cheeples Johnson <bc@worldofbilly.com>
2009-11-21 14:07:20 -0800

    Add permissions to the manifest and added bike logo.

--- gen/org/sfcta/cycletracks/R.java
+++ gen/org/sfcta/cycletracks/R.java
@@ -11,7 +11,8 @@ public final class R {
     public static final class attr {
     }
     public static final class drawable {
-        public static final int icon=0x7f020000;
+        public static final int bike=0x7f020000;
+        public static final int icon=0x7f020001;
     }
     public static final class id {
         public static final int ButtonStart=0x7f050000;
