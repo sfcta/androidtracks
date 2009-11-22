d832afba87b15159ab89ae444befeae1b43a7570
Cheeples Johnson <bc@worldofbilly.com>
2009-11-21 16:08:52 -0800

    Change start button to Button with drawable.

---
+++ src/org/sfcta/cycletracks/CycleTracksService.java
@@ -0,0 +1,19 @@
+package org.sfcta.cycletracks;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+public class CycleTracksService extends Service {
+
+	public CycleTracksService() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public IBinder onBind(Intent arg0) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
