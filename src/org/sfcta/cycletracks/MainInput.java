2f18c17acfbf8c92f9e5ca56ba2fc951f7aac16f
Cheeples Johnson <bc@worldofbilly.com>
2009-11-21 17:47:51 -0800

    Adding Button handling

--- src/org/sfcta/cycletracks/MainInput.java
+++ src/org/sfcta/cycletracks/MainInput.java
@@ -1,13 +1,24 @@
 package org.sfcta.cycletracks;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainInput extends Activity {
+public class MainInput extends Activity {    
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        final Button startButton = (Button) findViewById(R.id.ButtonStart);
+        Intent i = new Intent(this, RecordingActivity.class);
+        startButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+            	startActivity(i);
+            }
+        });
     }
 }
+
