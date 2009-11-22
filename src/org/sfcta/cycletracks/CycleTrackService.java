89fd131fcc900781ee0bf779991901dd337a3309
Cheeples Johnson <bc@worldofbilly.com>
2009-11-21 19:50:37 -0800

    Add buttons to the recording activity.

---
+++ src/org/sfcta/cycletracks/CycleTrackService.java
@@ -0,0 +1,15 @@
+package org.sfcta.cycletracks;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+public class CycleTrackService extends Service {
+
+	@Override
+	public IBinder onBind(Intent intent) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
