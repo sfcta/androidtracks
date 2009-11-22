55fa11ce28b03bfdee21d2a5286f80c9a10e42a0
Cheeples Johnson <bc@worldofbilly.com>
2009-11-21 17:48:09 -0800

    Adding Button handling

---
+++ src/org/sfcta/cycletracks/RecordingActivity.java
@@ -0,0 +1,12 @@
+package org.sfcta.cycletracks;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class RecordingActivity extends Activity {
+	@Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.recording);
+    }
+}
