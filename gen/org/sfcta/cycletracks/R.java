60ada8e9391c6016d4c8fe4fa562fabb24bd24e9
Cheeples Johnson <bc@worldofbilly.com>
2009-11-21 16:42:29 -0800

    Adding blank XML files for layout

--- gen/org/sfcta/cycletracks/R.java
+++ gen/org/sfcta/cycletracks/R.java
@@ -21,6 +21,9 @@ public final class R {
     }
     public static final class layout {
         public static final int main=0x7f030000;
+        public static final int recording=0x7f030001;
+        public static final int save=0x7f030002;
+        public static final int settings=0x7f030003;
     }
     public static final class string {
         public static final int app_name=0x7f040000;
