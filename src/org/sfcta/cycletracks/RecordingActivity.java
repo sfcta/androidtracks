5ccfb95d7613d6626240b4505ec95fb6ba452008
Billy Charlton <billy@snarfles.(none)>
2009-11-29 15:42:43 -0800

    Save trip screen added

--- src/org/sfcta/cycletracks/RecordingActivity.java
+++ src/org/sfcta/cycletracks/RecordingActivity.java
@@ -1,12 +1,23 @@
 package org.sfcta.cycletracks;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class RecordingActivity extends Activity {
 	@Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.recording);
+        final Button finishButton = (Button) findViewById(R.id.ButtonFinished);
+        final Intent i = new Intent(this, SaveTrip.class);
+        finishButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+            	startActivity(i);
+            }
+        });
+
     }
 }
---
+++ src/org/sfcta/cycletracks/SaveTrip.java
@@ -0,0 +1,16 @@
+package org.sfcta.cycletracks;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class SaveTrip extends Activity {
+	@Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.save);
+
+    }
+}
