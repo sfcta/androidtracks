ede35dd53e07de30950f69f9ebb5cc1f4daa3a9c
Billy Charlton <billy.charlton@sfcta.org>
2009-12-10 10:24:11 -0800

    Add GPL v3 notice to all source code

--- src/org/sfcta/cycletracks/UserInfoActivity.java
+++ src/org/sfcta/cycletracks/UserInfoActivity.java
@@ -1,3 +1,12 @@
+/**	 CycleTracks, (c) 2009 San Francisco County Transportation Authority
+ * 					  San Francisco, CA, USA
+ *
+ *   Licensed under the GNU GPL version 3.0.
+ *   See http://www.gnu.org/licenses/gpl-3.0.txt for a copy of GPL version 3.0.
+ *
+ * 	 @author Billy Charlton <billy.charlton@sfcta.org>
+ *
+ */
 package org.sfcta.cycletracks;
 
 import java.util.Map;
