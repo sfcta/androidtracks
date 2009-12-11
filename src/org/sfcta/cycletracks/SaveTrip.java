4e78cd2b1f8617439706674c9247847208c666ce
Billy Charlton <billy.charlton@sfcta.org>
2009-12-11 14:51:28 -0800

    Implement the trip purpose buttons

--- src/org/sfcta/cycletracks/SaveTrip.java
+++ src/org/sfcta/cycletracks/SaveTrip.java
@@ -11,7 +11,9 @@ package org.sfcta.cycletracks;
 
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
+import java.util.HashMap;
 import java.util.TimeZone;
+import java.util.Map.Entry;
 
 import android.app.Activity;
 import android.content.ComponentName;
@@ -21,14 +23,73 @@ import android.content.ServiceConnection;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.IBinder;
+import android.text.Html;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
+import android.widget.CompoundButton;
 import android.widget.EditText;
-import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.Toast;
+import android.widget.ToggleButton;
 
 public class SaveTrip extends Activity {
 	long tripid;
+	HashMap <Integer, ToggleButton> purpButtons = new HashMap<Integer,ToggleButton>();
+	String purpose;
+
+	HashMap <Integer, String> purpDescriptions = new HashMap<Integer, String>();
+
+	// Set up the purpose buttons to be one-click only
+	void preparePurposeButtons() {
+		purpButtons.put(R.id.ToggleCommute, (ToggleButton)findViewById(R.id.ToggleCommute));
+		purpButtons.put(R.id.ToggleSchool,  (ToggleButton)findViewById(R.id.ToggleSchool));
+		purpButtons.put(R.id.ToggleWorkRel, (ToggleButton)findViewById(R.id.ToggleWorkRel));
+		purpButtons.put(R.id.ToggleExercise,(ToggleButton)findViewById(R.id.ToggleExercise));
+		purpButtons.put(R.id.ToggleSocial,  (ToggleButton)findViewById(R.id.ToggleSocial));
+		purpButtons.put(R.id.ToggleShopping,(ToggleButton)findViewById(R.id.ToggleShopping));
+		purpButtons.put(R.id.ToggleErrand,  (ToggleButton)findViewById(R.id.ToggleErrand));
+		purpButtons.put(R.id.ToggleOther,   (ToggleButton)findViewById(R.id.ToggleOther));
+
+        purpDescriptions.put(R.id.ToggleCommute,
+			"<b>Commute:</b> this bike trip was primarily to get between home and your main workplace.");
+		purpDescriptions.put(R.id.ToggleSchool,
+			"<b>School:</b> this bike trip was primarily to go to or from school or college.");
+		purpDescriptions.put(R.id.ToggleWorkRel,
+			"<b>Work-Related:</b> this bike trip was primarily to go to or from a business related meeting, function, or work-related errand for your job.");
+		purpDescriptions.put(R.id.ToggleExercise,
+			"<b>Exercise:</b> this bike trip was primarily for exercise, or biking for the sake of biking.");
+		purpDescriptions.put(R.id.ToggleSocial,
+			"<b>Social:</b> this bike trip was primarily for going to or from a social activity, e.g. at a friend's house, the park, a restaurant, the movies.");
+		purpDescriptions.put(R.id.ToggleShopping,
+			"<b>Shopping:</b> this bike trip was primarily to purchase or bring home goods or groceries.");
+		purpDescriptions.put(R.id.ToggleErrand,
+			"<b>Errand:</b> this bike trip was primarily to attend to personal business such as banking, a doctor  visit, going to the gym, etc.");
+		purpDescriptions.put(R.id.ToggleOther,
+			"<b>Other:</b> if none of the other reasons applied to this trip, you can enter comments below to tell us more.");
+
+		CheckListener cl = new CheckListener();
+		for (Entry<Integer, ToggleButton> e: purpButtons.entrySet()) {
+			e.getValue().setOnCheckedChangeListener(cl);
+		}
+	}
+
+	// Called every time a purp togglebutton is changed:
+	class CheckListener implements CompoundButton.OnCheckedChangeListener {
+		@Override
+		public void onCheckedChanged(CompoundButton v, boolean isChecked) {
+			// First, uncheck all purp buttons
+			if (isChecked) {
+				for (Entry<Integer, ToggleButton> e: purpButtons.entrySet()) {
+					e.getValue().setChecked(false);
+				}
+				v.setChecked(true);
+				purpose = v.getText().toString();
+				((TextView) findViewById(R.id.TextPurpDescription)).setText(
+				        Html.fromHtml(purpDescriptions.get(v.getId())));
+			}
+		}
+	}
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -36,6 +97,10 @@ public class SaveTrip extends Activity {
 		setContentView(R.layout.save);
 
 		finishRecording();
+
+		// Set up trip purpose buttons
+		purpose = "";
+		preparePurposeButtons();
 
         // User prefs btn
         final Button prefsButton = (Button) findViewById(R.id.ButtonPrefs);
@@ -83,8 +148,13 @@ public class SaveTrip extends Activity {
 				TripData trip = TripData.fetchTrip(SaveTrip.this, tripid);
 				trip.populateDetails();
 
-				// Find user-entered info
-				Spinner purpose = (Spinner) findViewById(R.id.SpinnerPurp);
+				// Make sure trip purpose has been selected
+				if (purpose.equals("")) {
+					// Oh no!  No trip purpose!
+					Toast.makeText(getBaseContext(), "You must select a trip purpose before submitting! Choose from the purposes above.", Toast.LENGTH_SHORT).show();
+					return;
+				}
+
 				EditText notes = (EditText) findViewById(R.id.NotesField);
 
 				String fancyStartTime = DateFormat.getInstance().format(trip.startTime);
@@ -100,13 +170,17 @@ public class SaveTrip extends Activity {
 
 				// Save the trip details to the phone database. W00t!
 				trip.updateTrip(
-						purpose.getSelectedItem().toString(),
+						purpose,
 						fancyStartTime, fancyEndInfo,
 						notes.getEditableText().toString());
 				trip.updateTripStatus(TripData.STATUS_COMPLETE);
 				resetService();
 
-				// Now need to create the MainInput Activity so BACK btn works properly
+				// Force-drop the soft keyboard for performance
+				InputMethodManager imm = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
+				imm.hideSoftInputFromWindow(v.getWindowToken(), 0);
+
+				// Now create the MainInput Activity so BACK btn works properly
 				Intent i = new Intent(getApplicationContext(), MainInput.class);
 				startActivity(i);
 
