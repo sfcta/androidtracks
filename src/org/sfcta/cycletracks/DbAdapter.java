88385e2bc68dc150e43c922d882bf11a011086a6
Billy Charlton <billy.charlton@sfcta.org>
2010-05-04 16:35:22 -0700

    update copyright notices, but still under GNU GPL v3 as always

--- src/org/sfcta/cycletracks/DbAdapter.java
+++ src/org/sfcta/cycletracks/DbAdapter.java
@@ -1,12 +1,24 @@
-/**	 CycleTracks, (c) 2009 San Francisco County Transportation Authority
- * 					  San Francisco, CA, USA
- *
- *   Licensed under the GNU GPL version 3.0.
- *   See http://www.gnu.org/licenses/gpl-3.0.txt for a copy of GPL version 3.0.
+/**	 CycleTracks, Copyright 2009,2010 San Francisco County Transportation Authority
+ *                                    San Francisco, CA, USA
  *
  * 	 @author Billy Charlton <billy.charlton@sfcta.org>
  *
+ *   This file is part of CycleTracks.
+ *
+ *   CycleTracks is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   CycleTracks is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with CycleTracks.  If not, see <http://www.gnu.org/licenses/>.
  */
+
 package org.sfcta.cycletracks;
 
 import android.content.ContentValues;
