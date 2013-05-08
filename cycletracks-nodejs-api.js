75d961637e1bb86cadb6b37a82a6364c119d2162
Martin <commerce.github@remylabs.com>
2013-05-07 19:53:03 -0600

    Insert the data into a hosted cloudant couchdb instance.

--- cycletracks-nodejs-api.js
+++ cycletracks-nodejs-api.js
@@ -3,52 +3,56 @@
  * prepare to use the HTTP library
  */
 
-var    nano = require('nano')('http://127.0.0.1:5984')
-,    tracks = nano.use('tracks')
-,        qs = require('querystring')
-,      http = require('http')
-
-/* Normally, we won't serve any pages, but for testing purposes, here's some HTML to serve up
- */
-
-dummyHTML =
-  '<html><head><title>Node.js test page for Cycletracks API</title></head>' +
-  '<body>' +
-  '<form method="post">' +
-  'JSON:<input type="text" name="json">' +
-  '<input type="submit">' +
-  '</form></body></html>';
+var qs = require('querystring');
+var http = require('http');
+var tracks = require('nano')('https://pespecklyperilenlypitypi:syo5S0OaYP5L7BB5UEnqnd7K@martinremy.cloudant.com/cocycletracks');
 
 http.createServer(function (req, res) {
-  //Don't even touch other forms of requests.
+  // Only process POST requests (below, if non-POST, we show a debug form)
   if (req.method === "POST") {
     var body = "";
-    //Grab POST data one chunk at a time and aggregate.
+    // Grab POST data one chunk at a time and aggregate.
     req.on('data', function(chunk) { body += chunk; });
     req.on('end', function() {
-      //Filter out the odd empty request.
+      // Filter out the odd empty request.
       if (body) {
-        /* Instrumentation:
-         * console.log('Received via POST: ' + body);
-         */
+        console.log( 'Received via POST: ' + body );
+        // decodedBody = decodeURIComponent( body );
+        // console.log('Decoded: ' + decodedBody);
 
-        //CouchDB expects a genuine JSON object
-        var couchInput = qs.parse(body);
+        try {
+          // CouchDB expects a genuine JSON object
+          var couchInput = qs.parse(body);
+        } catch ( err ) {
+          console.log('Error: JSON parsing');
+          res.writeHead( 500, {'Content-Type': 'application/json'});
+          res.write( '{ "status":"error", "msg":"json parse error" }' );
+          return;
+        }
 
-        /* Instrumentation:
-         * console.log('Updating CouchDB with ' + JSON.stringify(couchInput));
-         */
+        console.log('Updating CouchDB with ' + JSON.stringify(couchInput));
 
-        //Create a more-or-less unique name for 
-        var epoch = new Date().getTime().toString();
+        // Create a (probably) unique name for 
+        var epoch = new Date().getTime().toString() + '_' + ( Math.floor( Math.random() * 1000 ) + 1 );
         console.log(epoch);
         tracks.insert(couchInput, epoch, function (err, body) { if (err) console.error(err); });
+        res.writeHead(200, {'Content-Type': 'application/json'});
+        res.write( '{"status":"success"}' );
       }
     });
+  } else {
+    // Not a POST request, show a debug form where you can paste JSON
+    dummyHTML =
+      '<html><head><title>Node.js test page for Cycletracks API</title></head>' +
+      '<body>' +
+      '<form method="post">' +
+      'JSON:<input type="text" name="json">' +
+      '<input type="submit">' +
+      '</form></body></html>';
+    res.writeHead(200, {'Content-Type': 'text/html'} );
+    res.end(dummyHTML);
   }
-  res.writeHead(200);
-  res.end(dummyHTML);
-}).listen(1337, '127.0.0.1');
-/* Last bit of instrumentation
- * console.log('Server running at http://127.0.0.1:1337/');
- */
+}).listen(1337);
+
+console.log('Server running at http://127.0.0.1:1337/');
+ 
